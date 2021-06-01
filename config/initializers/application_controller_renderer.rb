c3aa636c7265191966d2ee9931a7f08175fb41c7
sekitanaoki <naoki.seki20@gmail.com>
2021-06-01 07:28:48 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '6819e9cae3ab40969f76fd065728daec4764315dcb0bf4ec80d9ebc6a936d581f29ec262897ad5946b6c39ace554979d06d608872b95f3eb481e29f6a2cdaf9c'
